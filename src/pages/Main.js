fee14a064f45aa0f3abca8e272adfdd1abbdc9b8
O Y <orenyaniv90@gmail.com>
2022-03-28 12:31:41 +0300

    Init main page

---
+++ src/pages/Main.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+
+export const Main = () => {
+    return (
+        <Link to='game/gameI0001'>dymmyGame 1</Link>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
+import { Main } from "./pages/Main"
 import { Game } from "./pages/Game"
 
 export const routes = [
+    { path: '/', element: Main },
     { path: '/game/:id', element: Game }
 ]
